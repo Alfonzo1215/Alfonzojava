16555e2d070111a11fd62a51615cac3774fdebdb
Alfonzo1215 <70297697+Alfonzo1215@users.noreply.github.com>
2020-09-07 22:42:14 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var coolStuffEle = document.body.querySelector(".coolStuff");
-var prompter =prompt("How many miles do you drive per year?");
-var prompter =prompt("What is the current cost of a Gallon of gas");
+var prompter = prompt("How many miles do you drive per year?");
+var prompter = prompt("What is the current cost of a Gallon of gas");
 
 function rewritefunction(){
   var inputmiles = prompt("How many miles do you drive per year?");
