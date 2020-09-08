9c8f40e5664ea672784baba9004001a6202b208d
Alfonzo1215 <70297697+Alfonzo1215@users.noreply.github.com>
2020-09-07 21:54:34 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+var coolStuffEle = document.body.querySelector(".coolStuff");
+var prompter =prompt("How many miles do you drive per year?");
+var prompter =prompt("What is the current cost of a Gallon of gas");
+
+function rewritefunction(){
+  var inputmiles = prompt("How many miles do you drive per year?");
+  var inputgas = prompt("What is the current cost of a Gallon of gas");
+  var Averagecost = prompt("Average yearly cost based on MPG ");
+}
+
+
+  writefunction(inputmiles,inputgas);
+ 
+
+
+coolStuffEle.innerHTML=prompter;
